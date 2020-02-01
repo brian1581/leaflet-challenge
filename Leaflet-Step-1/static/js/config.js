d4ab2ca5fc294e77bd317633ec9377b1c300e0c8
Brian Abbe <54324229+brian1581@users.noreply.github.com>
2020-02-01 09:51:18 -0800

    Removing for API Key purposes

--- Leaflet-Step-1/static/js/config.js
+++
@@ -1,4 +0,0 @@
-// API key
-var API_KEY = 'pk.eyJ1IjoiYnJpYW4xNTgxIiwiYSI6ImNrNHp3Mjh1czBmMjUzbHF2b3J5M3IyZTUifQ.2_BUD4q_qTaJwOZsLCVjHg';
-const MAPBOX_URL = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
-const ATTRIBUTION = "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>";
